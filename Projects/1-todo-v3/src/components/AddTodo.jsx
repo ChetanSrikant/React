df59132ac95a1050db9510591406a7d7d654fb43
ChetanSrikant <chetansmandiga@gmail.com>
2024-09-22 21:06:28 +0530

    Todo finish

--- Projects/1-todo-v3/src/components/AddTodo.jsx
+++ Projects/1-todo-v3/src/components/AddTodo.jsx
@@ -1,44 +1,48 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useRef } from "react";
 
-const AddTodo = ({onNewItem}) => {
+const AddTodo = ({ onNewItem }) => {
+  // Use useRef to get references to the input elements
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
+  const handleAddButtonClicked = () => {
+    // Get the current value from the input fields
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
 
-  const handleNameChange = (event)=> {
-    setTodoName(event.target.value)
-  }
+    // Pass the values to the onNewItem function
+    onNewItem(todoName, dueDate);
 
-  const handleDateChange = (event)=> {
-    setDueDate(event.target.value)
-  }
-
-  const handleAddButtonClicked = (event)=>{
-    onNewItem(todoName, dueDate)
-    setDueDate("");
-    setTodoName("");
-  }
-  
+    // Clear the input fields
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+  };
 
   return (
-    <div>      
+    <div>
       <div className="row kg-row">
-          <div className="col-6">
-            <input type="text" name="" id="" placeholder="Enter Todo" value={todoName} onChange={handleNameChange}/>
-          </div>
-            <div className="col-4">
-              <input type="date" value={dueDate} onChange={handleDateChange}/>
-            </div>
-            <div className="col-2">
-              <button type="button" className="btn btn-success kg-button"
-              onClick={handleAddButtonClicked}>Add</button>
-            </div>
-          </div>
+        <div className="col-6">
+          <input
+            type="text"
+            ref={todoNameElement}
+            placeholder="Enter Todo"
+          />
+        </div>
+        <div className="col-4">
+          <input type="date" ref={dueDateElement} />
+        </div>
+        <div className="col-2">
+          <button
+            type="button"
+            className="btn btn-success kg-button"
+            onClick={handleAddButtonClicked}
+          >
+            Add
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
+};
 
-}
-
-
-export default AddTodo
+export default AddTodo;
