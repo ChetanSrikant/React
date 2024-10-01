31a6ded44abea331aae57996d1300fe390284913
ChetanSrikant <chetansmandiga@gmail.com>
2024-10-01 22:02:48 +0530

    useMemo

--- Learn/usememo/src/App.jsx
+++ Learn/usememo/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const nums = new Array(30_000_000).fill(0).map((_,i)=>{
+  return{
+    index: i,
+    isMagical : i === 29_000_000
+  }
+})
+
 function App() {
   const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState(nums)
+
+  // const magical = numbers.find(item=> item.isMagical== true)
+  const magical = useMemo(() => numbers.find(item=> item.isMagical== true), [])
 
   return (
     <>
       <div>
+        <span>Magical number is {magical.index}</span>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
